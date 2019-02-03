5fa69b98cbfb1be8f105f81dd7c88df4e803d50b
m2 malmstroem <m2@malmstroem.net>
2019-02-03 09:10:01 +0100

    simple appliappe - echos comment to stdout

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="appliapps",
-    version="0.0.1",
+    version="0.0.2",
     author="Lars Malmstroem",
     maintainer=['Lars Malmstroem'],
     author_email="lars@malmstroem.net",
