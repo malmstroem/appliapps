d42759a4b64aeccd979e317c3479f78f46bff3ca
m2 malmstroem <m2@malmstroem.net>
2019-02-03 09:04:38 +0100

    initial commot, readme, setup and .gitignore

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="appliapps",
+    version="0.0.1",
+    author="Lars Malmstroem",
+    maintainer=['Lars Malmstroem'],
+    author_email="lars@malmstroem.net",
+    maintainer_email=["lars@malmstroem.net"],
+    description="Applicake apps.",
+    license="BSD",
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/malmstroem/appliapps',
+    install_requires=[]
+)
